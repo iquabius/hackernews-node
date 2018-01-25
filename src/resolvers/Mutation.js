4c7761a94a7a6efd8a932363930d577fb6381e2b
Josias Iquabius <iquabius@gmail.com>
2018-01-25 11:45:27 -0300

    Add vote mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -43,8 +43,31 @@ async function login(parent, args, context, info) {
   }
 }
 
+async function vote(parent, args, context, info) {
+  const userId = getUserId(context)
+  const { linkId } = args
+  const linkExists = await context.db.exists.Vote({
+    user: { id: userId },
+    link: { id: linkId },
+  })
+  if (linkExists) {
+    throw new Error(`Already voted for link: ${linkId}`)
+  }
+
+  return context.db.mutation.createVote(
+    {
+      data: {
+        user: { connect: { id: userId } },
+        link: { connect: { id: linkId } },
+      },
+    },
+    info,
+  )
+}
+
 module.exports = {
   post,
   signup,
   login,
+  vote,
 }
